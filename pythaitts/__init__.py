dbcaeb044b421d19e2859f709a40f89351512607
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-07-19 00:54:52 -0700

    Add lunarlist_model

--- pythaitts/__init__.py
+++ pythaitts/__init__.py
@@ -6,9 +6,9 @@ __version__ = "0.1.1"
 
 
 class TTS:
-    def __init__(self, pretrained="khanomtan", mode="last_checkpoint", version="1.0") -> None:
+    def __init__(self, pretrained="khanomtan", mode="last_checkpoint", version="1.0", device:str="cpu") -> None:
         """
-        :param str pretrained: TTS pretrained (khanomtan)
+        :param str pretrained: TTS pretrained (khanomtan, lunarlist)
         :param str mode: pretrained mode
         :param str version: model version (default is 1.0 or 1.1)
 
@@ -18,9 +18,12 @@ class TTS:
         
         You can see more about khanomtan tts at `https://github.com/wannaphong/KhanomTan-TTS-v1.0 <https://github.com/wannaphong/KhanomTan-TTS-v1.0>`_
         and `https://github.com/wannaphong/KhanomTan-TTS-v1.1 <https://github.com/wannaphong/KhanomTan-TTS-v1.1>`_
+        
+        You can see more about lunarlist tts at `https://link.medium.com/OpPjQis6wBb <https://link.medium.com/OpPjQis6wBb>`_
         """
         self.pretrained = pretrained
         self.mode = mode
+        self.device = device
         self.load_pretrained(version=version)
 
     def load_pretrained(self,version):
@@ -30,6 +33,9 @@ class TTS:
         if self.pretrained == "khanomtan":
             from pythaitts.pretrained import KhanomTan
             self.model = KhanomTan(mode=self.mode, version=version)
+        elif self.pretrained == "lunarlist":
+            from pythaitts.pretrained import LunarlistModel
+            self.model = KhanomTan(mode=self.mode, device=self.device)
         else:
             raise NotImplemented(
                 "PyThaiTTS doesn't support %s pretrained." % self.pretrained
